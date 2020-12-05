c977f9f80735a4bd3f568c690ecf8d130644c3f8
Timothee Cour <timothee.cour2@gmail.com>
2020-12-05 02:34:07 -0800

    fixup

--- tests/core/tnimlint.nim
+++ tests/core/tnimlint.nim
@@ -6,6 +6,10 @@ proc main =
   const buildDir = currentSourcePath.parentDir.parentDir.parentDir / "build"
   createDir buildDir
   const fileOutput = buildDir / fileInput.lastPathPart
+  echo fileOutput
+  removeFile fileOutput
   main(fileInput, fileOutput)
+  doAssert fileOutput.fileExists, fileOutput
+  # TODO: check file content matches some groundtruth
 
 main()
