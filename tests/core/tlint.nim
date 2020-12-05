299c9d66f3ec6c169c58185ede5a4dcbeb423dd4
Timothee Cour <timothee.cour2@gmail.com>
2020-12-05 01:39:48 -0800

    support both testament and nimble test

--- tests/core/tlint.nim
+++ tests/core/tlint.nim
@@ -4,6 +4,7 @@ import lint
 import lintpkg/submodule
 doAssert getWelcomeMessage() == "Hello, World!" # palceholder
 
+# const root =  currentSourcePath.parentDir.parentDir.parentDir
 const fileInput = currentSourcePath.parentDir / "example.nim"
-const fileOutput = currentSourcePath.parentDir.parentDir / "build/example.nim"
+const fileOutput = currentSourcePath.parentDir.parentDir.parentDir / "build/example.nim"
 main(fileInput, fileOutput)
